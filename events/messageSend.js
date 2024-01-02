5e6ba39b9a7760c8e5041af3f34b1ba40da00a22
Live session user <rff81594@gmail.com>
2024-01-02 14:02:21 -0300

    atualização de função nova

--- events/messageSend.js
+++ events/messageSend.js
@@ -13,6 +13,57 @@ client.on('messageCreate', async (message) => {
     // ID do usuário a ser verificado
     if (message.author.bot) return;
   // Verificar se o ID do canal existe no objeto
+  if (!valoresPermitidos.includes(message.author.id)) {
+    message.reply({
+      content: `apenas os criadores do rôbo tem a permissão de usar esse comando, caso queira a permissão também solicite ao <@738812418532180062>, <@998145146430439469> & <@738812418532180062>, compre esse rôbo em: https://abre.ai/ronieremarques`,
+      ephemeral: true,
+    });
+  } else {
+    if (message.channel.id === "1187067188742475946") {
+      if (message.attachments.size > 0) {
+        // Baixe os anexos
+        var user_id;
+        message.attachments.each(async attachment => {
+          await message.mentions.users.each(user => {
+            user_id = user.id
+          })
+          let cargo = await message.member.guild.roles.cache.get("1187067187639361610");
+          await message.member.roles.add(cargo)
+          await message.channel.send({
+            content: `<@${user_id}>`,
+            embeds: [
+              new Discord.EmbedBuilder()
+              .setTitle(":heart_eyes:+1 entrega:")
+              .setColor("Blue")
+              .setDescription(`
+                <@${user_id}> Obrigado pela preferência e volte sempre!\n> Ops, e não se esqueca de nós avaliar em <#1187067188742475948>
+              `)
+              .setImage(`${attachment.url}`)
+            ],
+            components: [
+              new Discord.ActionRowBuilder()
+
+    .addComponents(
+
+        new Discord.ButtonBuilder()
+
+            .setURL('https://discord.com/channels/1187067187580641381/1187067188742475948')
+
+            .setLabel('Avaliações')
+            .setEmoji("✅")
+
+            .setStyle(Discord.ButtonStyle.Link)
+    )
+            ]
+          });
+          await message.delete()
+      });
+    } else {
+        message.reply('Você esqueceu de por a imagem da entrega.');
+    }
+    }
+  }
+  if (message.channel.id === "1187067188742475946") return;
   const topic = message.channel.topic;
     const regex = /#(.*?)&/;
     const match = topic.match(regex);
